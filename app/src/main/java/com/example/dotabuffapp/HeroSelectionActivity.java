6ed4920cd1b44ba46a487380e41b80a584959411
AlexD <alexander.drugakov@gmail.com>
2019-06-18 16:13:30 +0300

    App 0.0.2v

---
+++ app/src/main/java/com/example/dotabuffapp/HeroSelectionActivity.java
@@ -0,0 +1,14 @@
+package com.example.dotabuffapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HeroSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hero_selection_activity);
+    }
+}
--- app/src/main/java/com/example/dotabuffapp/MainActivity.java
+++ app/src/main/java/com/example/dotabuffapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.dotabuffapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
@@ -10,9 +12,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_activity);
 
         ViewPager pager = (ViewPager) findViewById(R.id.pager);
         pager.setAdapter(new MyAdapter(this, getSupportFragmentManager()));
+    }
+
+    public void selectHero(View view) {
+        Intent intent = new Intent(this, HeroSelectionActivity.class);
+        startActivity(intent);
     }
 }
